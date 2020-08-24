0bd3784d79a5ad7d662572b2d558202682b04f43
Gabriel Barros <bielb2@hotmail.com>
2020-08-24 17:06:46 -0300

    change toLocaleString

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ class Calculator {
         if (isNaN(integerDigits)) {
             integerDisplay = ''
         } else {
-            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+            integerDisplay = integerDigits.toLocaleString('pt', { maximumFractionDigits: 0 })
         }
         if (decimalDigits != null) {
             return `${integerDisplay}.${decimalDigits}`
